9214bf7df08f09b099be3003772eca2585440277
BuildTools <unconfigured@null.spigotmc.org>
2021-09-07 11:09:43 +0200

    Items toegevoegd

---
+++ error.py
@@ -0,0 +1,15 @@
+
+def foreach(classIn):
+    for cls in classIn.__subclasses__():
+        try:
+            raise cls()
+        except BaseException as e:
+            print(f'Mission failed successfully (Raised {e.__class__.__name__})')
+        finally:
+            foreach(cls)
+
+
+if __name__ == '__main__':
+    foreach(BaseException)
+    print("Done")
+    raise BaseException()
